2749a92c7c362c9980c5630010bf4c1003aab473
galaxydev <galaxydev13yt@gmail.com>
2026-03-30 19:18:07 -0500

    Add files via upload

---
+++ config.example.js
@@ -0,0 +1,11 @@
+window.PURESOUND_CONFIG = {
+  apiBase: "http://localhost:8787/api",
+  drive: {
+    rootFolderId: "1eBXiNU5vMlK67JELspL6_QCVQBDSwDJ2",
+  },
+  allowDirectApiKey: false,
+  directBrowser: {
+    apiKey: "",
+  },
+};
+
